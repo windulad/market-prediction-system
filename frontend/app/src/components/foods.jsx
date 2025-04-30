e995fe9c00c349476742b7681e557b66b9b138ca
ACER <charithwinwindula@gmail.com>
2025-05-01 02:41:47 +0530

    tailwind initizlized

---
+++ frontend/app/src/components/foods.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react";
+import './style.css';
+import { useNavigate } from 'react-router-dom';
+
+function Foods(){
+    return(
+        <p>Foods</p>
+    )
+}
+
+export default Foods;
