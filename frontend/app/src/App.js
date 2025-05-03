f25e119687ef20828ccbff7d46cec1677234c17f
ACER <charithwinwindula@gmail.com>
2025-05-03 20:51:41 +0530

    new changes home, restaurant

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -3,9 +3,9 @@ import React, { useState,useEffect } from "react";
 import Home from './components/home';
 import LogIn from './components/login';
 import SignUp from './components/signup';
-import Foods from './components/foods';
-import Clothes from './components/clothes';
-import Grooming from './components/grooming';
+import Restaurants from './components/restaurants';
+import Fashion from './components/fashion';
+import Wellness from './components/wellness';
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -14,9 +14,9 @@ function App() {
         <Route path="/" element={<Home/>}/>
         <Route path="/login" element={<LogIn/>}/>
         <Route path="/signup" element={<SignUp/>}/>
-        <Route path="/foods" element={<Foods/>}/>
-        <Route path="/clothes" element={<Clothes/>}/>
-        <Route path="/grooming" element={<Grooming/>}/>
+        <Route path="/restaurants" element={<Restaurants/>}/>
+        <Route path="/fashion" element={<Fashion/>}/>
+        <Route path="/wellness" element={<Wellness/>}/>
     </Routes>
   );
 }
---
+++ frontend/app/src/components/fashion.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from 'react-router-dom';
+
+function Fashion(){
+    return(
+        <p>Fashion</p>
+    )
+}
+
+export default Fashion;
