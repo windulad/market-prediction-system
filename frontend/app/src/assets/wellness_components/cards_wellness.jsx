2f27f65a599f9da2179c9e266d8044ef073124f2
ACER <charithwinwindula@gmail.com>
2025-05-04 22:59:19 +0530

    wellness completed

---
+++ frontend/app/src/assets/wellness_components/cards_wellness.jsx
@@ -0,0 +1,177 @@
+import Card1Img from './wellness_cards_images/card_1.jpg'
+import Card2Img from './wellness_cards_images/card_2.jpg'
+import Card3Img from './wellness_cards_images/card_3.jpg'
+
+const restaurants = [
+    {
+		id: 1,
+		name: "The Gourmet Grill",
+		location: "Colombo, Sri Lanka",
+		rating: 4.5,
+		image: Card1Img,
+		description: "A cozy urban hideout offering a fusion of Sri Lankan and Mediterranean flavors, perfect for brunch dates and late-night bites alike.",
+		cuisine: "Western",
+		price: "$$",
+  		isOpen: true
+	},
+	{
+		id: 2,
+		name: "Spice Hub",
+		location: "Kandy, Sri Lanka",
+		rating: 4.2,
+		image: Card2Img,
+		description: "Modern bistro serving handcrafted burgers, gourmet pasta, and locally sourced desserts in a warm, industrial-chic atmosphere.",
+		cuisine: "Italian",
+		price: "$$$",
+  		isOpen: true
+	},
+	{
+		id: 3,
+		name: "Ocean Breeze Café",
+		location: "Galle, Sri Lanka",
+		rating: 4.8,
+		image: Card3Img,
+		description: "Family-friendly diner with a vibrant menu of Asian street food, sizzling grills, and refreshing mocktails, just minutes from the city center.",
+		cuisine: "Vegetarian",
+		price: "$$$",
+  		isOpen: false
+	},
+	{
+		id: 4,
+		name: "The Gourmet Grill",
+		location: "Colombo, Sri Lanka",
+		rating: 4.5,
+		image: Card1Img,
+		description: "A cozy urban hideout offering a fusion of Sri Lankan and Mediterranean flavors, perfect for brunch dates and late-night bites alike.",
+		cuisine: "Western",
+		price: "$$",
+  		isOpen: true
+	},
+	{
+		id: 5,
+		name: "Spice Hub",
+		location: "Kandy, Sri Lanka",
+		rating: 4.2,
+		image: Card2Img,
+		description: "Modern bistro serving handcrafted burgers, gourmet pasta, and locally sourced desserts in a warm, industrial-chic atmosphere.",
+		cuisine: "Italian",
+		price: "$$$",
+  		isOpen: true
+	},
+	{
+		id: 6,
+		name: "Ocean Breeze Café",
+		location: "Galle, Sri Lanka",
+		rating: 4.8,
+		image: Card3Img,
+		description: "Family-friendly diner with a vibrant menu of Asian street food, sizzling grills, and refreshing mocktails, just minutes from the city center.",
+		cuisine: "Vegetarian",
+		price: "$$",
+  		isOpen: false
+	},
+	{
+		id: 7,
+		name: "The Gourmet Grill",
+		location: "Colombo, Sri Lanka",
+		rating: 4.5,
+		image: Card1Img,
+		description: "A cozy urban hideout offering a fusion of Sri Lankan and Mediterranean flavors, perfect for brunch dates and late-night bites alike.",
+		cuisine: "Western",
+		price: "$$",
+  		isOpen: true
+	},
+	{
+		id: 8,
+		name: "Spice Hub",
+		location: "Kandy, Sri Lanka",
+		rating: 4.2,
+		image: Card2Img,
+		description: "Modern bistro serving handcrafted burgers, gourmet pasta, and locally sourced desserts in a warm, industrial-chic atmosphere.",
+		cuisine: "Italian",
+		price: "$$$",
+  		isOpen: true
+	},
+	{
+		id: 9,
+		name: "Ocean Breeze Café",
+		location: "Galle, Sri Lanka",
+		rating: 4.8,
+		image: Card3Img,
+		description: "Family-friendly diner with a vibrant menu of Asian street food, sizzling grills, and refreshing mocktails, just minutes from the city center.",
+		cuisine: "Vegetarian",
+		price: "$$$",
+  		isOpen: false
+	},
+	{
+		id: 10,
+		name: "The Gourmet Grill",
+		location: "Colombo, Sri Lanka",
+		rating: 4.5,
+		image: Card1Img,
+		description: "A cozy urban hideout offering a fusion of Sri Lankan and Mediterranean flavors, perfect for brunch dates and late-night bites alike.",
+		cuisine: "Western",
+		price: "$$",
+  		isOpen: true
+	},
+];
+  
+function WellnessCards() {
+    return (
+		<div className="space-y-6">
+			{restaurants.map((restaurant) => (
+			<div key={restaurant.id}
+			className="flex bg-white w-full h-60 border shadow-md rounded-xl overflow-hidden hover:shadow-lg transition"
+			>
+				<img src={restaurant.image} alt={restaurant.name} 
+				className="w-80 h-60 border rounded-xl object-cover" 
+				/>
+				<div className="px-6 py-4 flex flex-col justify-start">
+					<div>
+						<h2 className="text-2xl font-bold">{restaurant.name}</h2>
+						<p className="text-sm font-medium text-gray-500">{restaurant.location}</p>
+						<div className="flex items-center space-x-1 pt-2">
+							{[...Array(5)].map((_, i) => (
+								<svg
+									key={i}
+									className={`w-5 h-5 ${i < restaurant.rating ? "text-yellow-400" : "text-gray-300"}`}
+									fill="currentColor"
+									viewBox="0 0 20 20"
+								>
+									<path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.184 3.63a1 1 0 00.95.69h3.813c.969 0 1.371 1.24.588 1.81l-3.087 2.24a1 1 0 00-.364 1.118l1.184 3.63c.3.921-.755 1.688-1.54 1.118l-3.087-2.24a1 1 0 00-1.175 0l-3.087 2.24c-.784.57-1.838-.197-1.539-1.118l1.184-3.63a1 1 0 00-.364-1.118L2.32 9.057c-.783-.57-.38-1.81.588-1.81h3.813a1 1 0 00.95-.69l1.184-3.63z"/>
+								</svg>
+							))}
+							<span className="text-sm text-yellow-500 font-medium">({restaurant.rating}/5)</span>
+						</div>
+						<p className="text-sm mt-2 text-gray-600">
+							{restaurant.description}
+						</p>
+						
+						<div className="flex justify-between items-center space-x-4 text-sm text-gray-500 pt-8">
+							<div>
+								<span className="bg-gray-100 px-3 mx-2 py-2 rounded-md">
+									{restaurant.cuisine}
+								</span>
+								<span className="bg-gray-100 px-3 mx-2 py-2 rounded-md">
+									{restaurant.price}
+								</span>
+								<span className={`px-3 mx-2 py-2 rounded-md ${restaurant.isOpen ? 'bg-green-100 text-green-600' : 'bg-red-100 text-red-600'}`}>
+									{restaurant.isOpen ? 'Open now' : 'Closed'}
+								</span>
+							</div>
+							<div>
+								<button className="text-sm px-3 py-2 rounded-md text-white bg-indigo-500 font-medium hover:bg-indigo-600">
+									More details
+								</button>
+							</div>
+							
+							
+						</div>
+					</div>
+					
+				</div>
+			</div>
+			))}
+		</div>
+    );
+}
+export default WellnessCards;
--- frontend/app/src/components/wellness.jsx
+++ frontend/app/src/components/wellness.jsx
@@ -1,9 +1,144 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from 'react-router-dom';
+import TitleImg from "../assets/wellness_components/titleimg.png"
+import FilterSideBarWellness from "../assets/wellness_components/filtersidebar_wellness";
+import WellnessCards from "../assets/wellness_components/cards_wellness"
 
 function Wellness(){
+    const country = [
+        { label: "Sri Lanka", value: "country1" }
+    ];
+
+    const city = [
+        { label: "Colombo", value: "city1" },
+        { label: "Kandy", value: "city2" },
+        { label: "Galle", value: "city3" }
+    ];
+
+    const [selectedCountry, setSelectedCountry] = useState("country1");
+
+    const getCountryLabel = (value) => {
+        const match = country.find((loc) => loc.value === value);
+        return match ? match.label : "";
+    };
+
+    const [selectedCity, setSelectedCity] = useState("city1");
+
+    const getCityLabel = (value) => {
+        const match = city.find((loc) => loc.value === value);
+        return match ? match.label : "";
+    };
+
     return(
-        <p>Wellness</p>
+        <div className="overflow-x-hidden">
+            {/* Navigation */}
+            <nav className="fixed top-0 left-0 right-0 z-50 bg-white">
+                <div className="max-w-screen-xl mx-auto py-2">
+                    <div className="flex h-16 justify-between items-center">
+                        <div className="flex space-x-8 items-center">
+                            <a href="#" className="text-3xl text-indigo-500 font-bold">Moodify</a>
+                            <div class="flex items-center w-full max-w-xl bg-white rounded-full shadow overflow-hidden">
+                                <select class="flex-grow w-32 px-4 py-2 text-md text-gray-700 placeholder-gray-500 focus:outline-none appearance-none bg-white"
+                                value={selectedCountry}
+                                onChange={(e) => setSelectedCountry(e.target.value)}>
+                                    {country.map((loc) => (
+                                        <option key={loc.value} value={loc.value}>
+                                            {loc.label}
+                                        </option>
+                                    ))}
+                                </select>
+                                <div class="h-6 w-px bg-gray-300 mx-2"></div>
+                                <select class="w-40 px-2 py-2 text-md text-gray-700 placeholder-gray-500 focus:outline-none appearance-none bg-white"
+                                value={selectedCity}
+                                onChange={(e) => setSelectedCity(e.target.value)}>
+                                    {city.map((loc) => (
+                                        <option key={loc.value} value={loc.value}>
+                                            {loc.label}
+                                        </option>
+                                    ))}
+                                </select>
+                                <button class="bg-indigo-500 hover:bg-indigo-700 p-3 flex items-center justify-center">
+                                    <svg class="h-4 w-4 text-white" fill="currentColor" viewBox="0 0 20 20">
+                                        <path
+                                            fill-rule="evenodd"
+                                            d="M12.9 14.32a8 8 0 111.414-1.414l4.387 4.387a1 1 0 01-1.414 1.414l-4.387-4.387zM14 8a6 6 0 11-12 0 6 6 0 0112 0z"
+                                            clip-rule="evenodd"
+                                        />
+                                    </svg>
+                                </button>
+                            </div>
+                            <a href="#" className="text-lg text-gray-600 hover:text-indigo-600 font-semibold">Restaurants</a>
+                            <a href="#" className="text-lg text-gray-600 hover:text-indigo-600 font-semibold">Fashion</a>
+                            <a href="#" className="text-lg text-gray-600 hover:text-indigo-600 font-semibold">Wellness</a>
+                            <a href="#" className="text-lg text-gray-600 hover:text-indigo-600 font-semibold">Moodify&nbsp;for&nbsp;Buiness</a>
+                        </div>
+                        <div className="flex space-x-8 items-center">
+                            <a href="#" className="text-lg text-gray-600 hover:text-indigo-600 font-semibold">Log&nbsp;In</a>
+                            <a href="#" className="text-lg px-3 py-2 rounded-md text-white bg-indigo-500 hover:bg-indigo-600 font-semibold">Sign&nbsp;Up</a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+
+            {/* Title, Subtitle */}
+            <div className="fixed top-2 left-0 right-0 bg-white z-40 shadow-md px-4 pb-4">
+                <div className="flex items-center justify-between">
+                    <div className="text-left pt-20 pb-4 mx-10">
+                        <h2 className="text-4xl font-bold mb-2">Top Wellness Spots in {getCityLabel(selectedCity)}, {getCountryLabel(selectedCountry)}</h2>
+                        <p className="text-sm font-semibold">What’s Trending in {getCityLabel(selectedCity)} – Best Wellness Destinations Right Now</p>
+                    </div>
+                    <div className="ml-4 pt-20 pb-4 mx-10">
+                        <img src={TitleImg} className="w-20 h-20"/>
+                    </div>
+                </div>
+            </div>
+
+            <div className="flex pt-52 z-30">
+                {/* Filters */}
+                <aside className="w-90 pt-4 pl-6 pr-4 -mt-4 border-l border-gray-200 shadow-[4px_0_10px_-2px_rgba(0,0,0,0.1)]">
+                    <FilterSideBarWellness />
+                </aside>
+                
+                {/* Cards */}
+                <div className="flex-1 mt-4 pl-14 pr-12 py-2 pb-10">
+                    <WellnessCards />
+                </div>
+            </div>
+
+
+            {/* Footer */}
+            <footer className="bg-gray-900 text-white pt-10 pb-5 z-20">
+                <div className="max-w-7xl mx-auto px-6 grid grid-cols-1 md:grid-cols-3 space-x-8 gap-8">
+                    <div>
+                        <h3 className="text-2xl font-bold mb-2">Moodify</h3>
+                        <p className="text-sm text-gray-400">
+                            Real reviews from real people. Discover the best in food, fashion, and grooming with confidence.
+                        </p>
+                    </div>
+                    <div>
+                        <h4 className="text-md font-semibold mb-3">Quick Links</h4>
+                        <ul className="space-y-2 text-sm text-gray-300">
+                            <li><a href="#" className="hover:text-white">Home</a></li>
+                            <li><a href="#" className="hover:text-white">Write a Review</a></li>
+                            <li><a href="#" className="hover:text-white">About Us</a></li>
+                            <li><a href="#" className="hover:text-white">Terms of Service</a></li>
+                            <li><a href="#" className="hover:text-white">Privacy Policy</a></li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h4 className="text-md font-semibold mb-3">Follow Us</h4>
+                        <div className="flex space-x-4 text-gray-400">
+                            <a href="#"><svg className="w-5 h-5 hover:text-white" fill="currentColor" viewBox="0 0 24 24"><path d="M22.23 5.924a8.847 8.847 0 01-2.546.698 4.432 4.432 0 001.946-2.448 8.868 8.868 0 01-2.812 1.074A4.418 4.418 0 0015.447 4c-2.452 0-4.44 1.987-4.44 4.437 0 .348.038.687.11 1.011A12.553 12.553 0 013.1 5.13a4.418 4.418 0 001.372 5.915A4.39 4.39 0 012.8 10v.056a4.437 4.437 0 003.56 4.351 4.43 4.43 0 01-1.167.156c-.286 0-.563-.027-.833-.08a4.444 4.444 0 004.147 3.079 8.885 8.885 0 01-6.566 1.835A12.526 12.526 0 008.29 21c8.153 0 12.605-6.754 12.605-12.605 0-.192-.004-.384-.012-.574A9.01 9.01 0 0024 5.491a8.938 8.938 0 01-2.57.703 4.42 4.42 0 001.942-2.45 8.875 8.875 0 01-2.77 1.08z" /></svg></a>
+                            <a href="#"><svg className="w-5 h-5 hover:text-white" fill="currentColor" viewBox="0 0 24 24"><path d="M22.675 0h-21.35C.596 0 0 .592 0 1.324v21.352C0 23.408.596 24 1.325 24H12.82v-9.294H9.692V11.01h3.128V8.412c0-3.1 1.893-4.788 4.659-4.788 1.325 0 2.463.099 2.794.143v3.24l-1.918.001c-1.504 0-1.795.715-1.795 1.763v2.312h3.588l-.467 3.696h-3.121V24h6.116C23.404 24 24 23.408 24 22.676V1.324C24 .592 23.404 0 22.675 0z" /></svg></a>
+                            <a href="#"><svg className="w-5 h-5 hover:text-white" fill="currentColor" viewBox="0 0 24 24"><path d="M12 2.04c-5.51 0-9.96 4.45-9.96 9.96 0 5.51 4.45 9.96 9.96 9.96 5.51 0 9.96-4.45 9.96-9.96 0-5.51-4.45-9.96-9.96-9.96zm0 18.24c-4.564 0-8.28-3.716-8.28-8.28s3.716-8.28 8.28-8.28 8.28 3.716 8.28 8.28-3.716 8.28-8.28 8.28zm3.666-11.736c-.404 0-.782.09-1.127.246a2.165 2.165 0 00-.642.492c-.158.19-.28.407-.357.641a2.132 2.132 0 00-.135.816c0 .284.05.561.144.825a2.165 2.165 0 00.357.641c.158.19.353.35.57.482.218.13.457.23.707.296a2.142 2.142 0 001.418-.049 2.133 2.133 0 00.57-.296 2.165 2.165 0 00.642-.492c.158-.19.28-.407.357-.641.094-.264.144-.541.144-.825s-.05-.561-.144-.825a2.165 2.165 0 00-.357-.641 2.133 2.133 0 00-.57-.492 2.142 2.142 0 00-1.053-.246zm0 7.344h-7.332v-1.56h7.332v1.56z"/></svg></a>
+                        </div>
+                    </div>
+                </div>
+                <div className="mt-10 border-t border-gray-700 pt-4 text-center text-sm text-gray-500">
+                    © 2025 Moodify. All rights reserved.
+                </div>
+            </footer>
+        </div>
     )
 }
 
